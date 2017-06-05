2894c0f0e8300b41cbb7ad0f59adeffef550292b
Roberto <roberto.martins.info@gmail.com>
2017-06-05 09:47:28 -0300

    Adicionando css e iniciando a funcionalidade de loop

--- form.php
+++ form.php
@@ -1,22 +1,58 @@
-<?php 
+<?php
 ?>
 <html>
 <head>
 	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
-<select id="template">
-<?php foreach($templates as $template ):?>
-	<option value="<?php echo $template; ?>"><?php echo $template; ?></option>
-<?php endforeach;?>
-</select>
-<script type="text/javascript">
-	$("#template").change(function(){
-		$("#frmTemplate").load( "montaForm.php?form=" + $(this).find(':selected').val());
-	});
-</script>
+<body>
+	<div class="container">
+		<br><br>
+		<nav>
+			<ul class="nav nav-pills">
+				<li role="presentation" class="dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+						Dropdown <span class="caret"></span>
+					</a>
+					<ul class="dropdown-menu">
+					</ul>
+				</li>
+				<li role="presentation">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+						Template <span class="caret"></span>
+					</a>
+					<ul class="dropdown-menu">
+						<?php foreach($templates as $template ):?>
+							<li>
+								<a href="#template_<?php echo $template; ?>" onclick="changeTemplate(this)" data="<?php echo $template; ?>" ><?php echo $template; ?></a>
+							</li>
+						<?php endforeach;?>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+		<script type="text/javascript">
+			function changeTemplate(element){
+				$("#frmTemplate").load( "montaForm.php?form=" + $(element).attr('data'));
+			}
+			function refreshTemplate(element){
+				// $('.template_item').keyup(function(){
+				// 	$("#frmTemplate").load( "montaForm.php?form=" + $("input[name=form]").val());
+				// });
+			}
+		</script>
 
-<form id="frmTemplate">
-	<?php include "montaForm.php";?>
-</form>
-
+		<form id="frmTemplate" class="form-inline">
+			<?php include "montaForm.php";?>
+		</form>
+	</div>
+</body>
+</html>
 
--- montaForm.php
+++ montaForm.php
@@ -6,19 +6,60 @@
 	preg_match_all('/{{[a-zA-Z0-9]+}}/', $arquivo, $valores);
 
 	foreach ($valores[0] as $key => $value) {
+		
+
+		if($value == '{{loop}}'){
+			$in_loop = true;
+			$first_item_loop = true;
+		}elseif($value == '{{endloop}}'){
+			$last_item_loop = true;
+		}
+
 		?>
-		<br> 
-		<br> 
-		<?php echo $value; ?>:
-		<br> 
-		<input type="text" name="<?php echo $value ?>" value="<?php echo $_GET['' . $value . '']; ?>">
-		<?php 
+
+		<?php if($in_loop == true && $first_item_loop == true): ?>
+			<div class="row">
+				<div class="container">
+					<br><br>
+					Loop <br>
+		<?php endif;?>
+		<?php
+			$first_item_loop = false;
+		?>
+		<?php if($in_loop == true): ?>
+			<div class="form-group">
+		<?php endif;?>
+					<label class="sr-only" for="inlineFormInput"><?php echo $value; ?></label>
+					<input
+						style="margin-top: 12px"
+						type="text"
+						onchange="refreshTemplate(this);"
+						onpaste="this.onchange();" oninput="this.onchange();"
+						class="form-control mb-2 mr-sm-2 mb-sm-0 template_item" placeholder="<?php echo $value ?>" name="<?php echo $value ?>" value="<?php echo $_GET['' . $value . '']; ?>">
+		<?php if($in_loop == true): ?>
+			</div>
+		<?php endif;?>
+
+		<?php if($in_loop == true && $last_item_loop == true): ?>
+
+				<a onclick="" class="btn btn-primary" role="button"><i class="glyphicon glyphicon-plus"></i></a>
+
+				</div>
+			</div>
+		<?php endif;?>
+
+		<?php
+		$last_item_loop = false;
+		if($value == '{{endloop}}'){
+			$in_loop = false;
+		}
 	}
+
 
 ?>
 
 <br><br>
-<input type="submit" name="" value="Gerar">
+<input type="submit" name="" value="Gerar" class="btn btn-primary btn-lg">
 
 
 <?php 
